3b98716e8d576180c75baedcd986e6d785d724cc
griffan <zhangfanqw@gmail.com>
2015-12-25 14:31:27 -0500

    adding ks p value calculator

---
+++ pbwtWrapper/ks.h
@@ -0,0 +1,27 @@
+//
+// Created by Fan Zhang on 12/23/15.
+//
+
+#ifndef PLUTO_KS_H
+#define PLUTO_KS_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+//#include <R.h>
+//#include <Rinternals.h>
+//#include <Rmath.h>		/* constants */
+#ifndef M_1_SQRT_2PI
+#define M_1_SQRT_2PI	0.398942280401432677939946059934	/* 1/sqrt(2pi) */
+#endif
+
+double K(int n, double d);
+void m_multiply(double *A, double *B, double *C, int m);
+void m_power(double *A, int eA, double *V, int *eV, int m, int n);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif //PLUTO_KS_H
